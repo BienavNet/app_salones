9fdbac4cdc0fc43c6e74736dd66ef818155e6d1e
Carlos Sanchez Carrascal <105553158+carsanjs@users.noreply.github.com>
2024-07-25 05:55:52 -0500

    Dev frontend (#41)
    * agregando access token y refresh token
    
    * change
    
    * modifi
    
    * update
    
    * .
    
    * update changes commit remoto after main

--- src/assets/validation.js
+++ src/assets/validation.js
@@ -1,23 +1,27 @@
 export class Validaciones {
     static nombre(nombre) {
         if (typeof nombre !== 'string') throw new Error('Nombre debe ser una cadena de texto');
+        if (nombre.trim().length === 0) throw new Error('Nombre no puede estar vacío');
         if (nombre.length < 3) throw new Error('Nombre debe tener al menos 3 caracteres');
         if (nombre.length > 30) throw new Error('Nombre no puede ser mas de 30 caracteres');
     }
 
     static apellido(apellido) {
         if (typeof apellido !== 'string') throw new Error('Apellido debe ser una cadena de texto');
+        if (apellido.trim().length === 0) throw new Error('Apellido no puede estar vacío');
         if (apellido.length < 3) throw new Error('Apellido debe tener al menos 3 caracteres');
         if (apellido.length > 30) throw new Error('Nombre no puede ser mas de 30 caracteres');
     }
 
     static cedula(cedula) {
+        if (cedula.trim().length === 0) throw new Error('Cedula no puede estar vacío');
         if (typeof cedula !== 'number'|| !Number.isInteger(cedula)) throw new Error('Cédula debe ser un número');
         if (cedula.toString().length > 10) throw new Error('La cédula debe tener no puede ser mayor a 10 caracteres');
         if (cedula.toString().length < 8) throw new Error('La cédula debe tener no puede ser menor a 10 caracteres');
     }
 
     static correo(correo) {
+        if (correo.trim().length === 0) throw new Error('Correo no puede estar vacío');
         const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
         if (typeof correo !== 'string') throw new Error('Correo debe ser una cadena de texto');
         if (!emailRegex.test(correo)) throw new Error('Correo debe tener un formato válido');
@@ -26,8 +30,16 @@ export class Validaciones {
     }
 
     static contrasena(contrasena) {
+        if (contrasena.trim().length === 0) throw new Error('Contraseña no puede estar vacío');
         if (typeof contrasena !== 'string') throw new Error('Contraseña debe ser una cadena de texto');
         if (contrasena.length < 4) throw new Error('Contraseña debe tener al menos 6 caracteres');
         if (contrasena.length > 16) throw new Error('Contraseña debe no debe ser mayor a 16 caracteres');
+    }
+
+    static role(rol){
+        const validRoles = ['docente', 'admin', 'supervisor']
+        if (rol.trim().length === 0) throw new Error('Rol no puede estar vacío');
+        if (typeof rol !== 'string') throw new Error('rol debe ser una cadena de texto')
+        if(!validRoles.includes(rol)) throw new Error('Por favor, verfique su rol asignado');
     }
   }
--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,74 +1,71 @@
+import { Validaciones } from "../assets/validation.js";
 import { methods as database } from "./../database/database.js";
 import { tokensMethods as tokens } from "./../functions.js";
 
-
-
-
 const checkLogin = async (req, res) => {
+  if (!req.body || typeof req.body !== 'object' || Object.keys(req.body).length === 0)
+    res.status(400).json({
+      status: "Bad Request.",
+      message: "El cuerpo de la peticion no puede estar vacia.",
+    });
+  try {
+    const { correo, contrasena, rol } = req.body;
     try {
-        if (req.body !== undefined && req.body !== "") {
-            const { correo, contrasena, rol } = req.body
-            // const { access_token } = req.cookies
-
-
-            // // se verifica con las cookies que el usuario trata de loggear o al menos de cargar la pagina de login
-            // // si las cookies existen las validan y redireccionan al rol correspondiente
-            // if (typeof access_token !== "undefined"){
-            //     const data_decoded = tokens.verifyToken(access_token)
-            //     if (typeof data_decoded !== "undefined"){
-            //         const { user, rol } = data_decoded
-            //         if (typeof user !== "undefined" && typeof rol !== "undefined"){
-            //             res.set({
-            //                 "Authorization": access_token
-            //             })
-            //             res.status(200).json({ "status": "ok", "message": "Login Correcto.!"})
-            //             return
-            //         }
-            //     }
-                    
-
-            // }
-
-            if (correo !== "" && contrasena !== "" && rol !== "") {
-                const connection = await database.getConnection()
-                var result = await connection.query("SELECT id FROM persona WHERE correo = '" + correo + "' AND contrasena = '" + contrasena + "'")
-                const { id } = result[0]
-
-                if (id !== undefined && id !== "") {
-                    var result = await connection.query("SELECT id AS id_rol FROM " + rol + " WHERE persona = " + id + "")
-                    if (result.length > 0) {
-                        const { id_rol } = result[0]
-                        if (id_rol !== undefined && id_rol !== "") {
-                            const token = tokens.signToken({ user: correo, rol: rol })
-                            res.set({
-                                "Authorization": token
-                            })
-                            res.cookie("access_token", token, {
-                                httpOnly: true, // la cookie solo se puede acceder en el servidor
-                                // secure: process.env.NODE_ENV === 'production', // solo en el entorno de producción, la cookie solo se puede acceder desde https
-                                sameSite: 'strict', // la cookie solo se puede acceder en el mismo dominio
-                                maxAge: 1000 * 60 * 60 // la cookie tiene tiempo de valides de solo 1 hora
-                            })
-                            res.status(200).json({ "status": "ok", "message": "Login Correcto.!", "token": token })
-                            return
-                        }
-                    }
-                    res.status(200).json({ "status": "error", "message": "Error al determinar el rol." })
-                    return
-                }
-                res.status(200).json({ "status": "error", "message": "Credenciales incorrectas." })
-                return
-            } else {
-                res.status(400).json({ "status": "error", "message": "Revisa que los campos no esten vacios." })
-            }
-            res.status(400).json({ "status": "error", "message": "El cuerpo de la peticion no puede estar vacia." })
-            return
-        }
-    } catch (error) {
-        res.status(500).send('Internal Server Error: ' + error.message)
+      Validaciones.correo(correo);
+      Validaciones.contrasena(contrasena);
+      Validaciones.role(rol);
+    } catch (validationError) {
+      return res
+        .status(400)
+        .json({ status: "Bad Request", message: validationError.message });
     }
-}
+    const connection = await database.getConnection();
+    var result = await connection.query(
+      "SELECT id FROM persona WHERE correo = '" +
+        correo +
+        "' AND contrasena = '" +
+        contrasena +
+        "'"
+    );
+    const { id } = result;
+    console.log(id, "id" + "  data", result);
+
+    if (id !== undefined && id !== "") {
+      var result = await connection.query(
+        "SELECT id AS id_rol FROM " + rol + " WHERE persona = " + id + ""
+      );
+      if (result.length > 0) {
+        const { id_rol } = result[0];
+        if (id_rol !== undefined && id_rol !== "") {
+          const token = tokens.signToken({ user: correo, rol: rol });
+          res.set({
+            Authorization: token,
+          });
+          res.cookie("access_token", token, {
+            httpOnly: true, // la cookie solo se puede acceder en el servidor
+            // secure: process.env.NODE_ENV === 'production', // solo en el entorno de producción, la cookie solo se puede acceder desde https
+            sameSite: "strict", // la cookie solo se puede acceder en el mismo dominio
+            maxAge: 1000 * 60 * 60, // la cookie tiene tiempo de valides de solo 1 hora
+          });
+          res
+            .status(200)
+            .json({ status: "ok", message: "Login Correcto.!", token: token });
+          return;
+        }
+      }
+      res
+        .status(200)
+        .json({ status: "error", message: "Error al determinar el rol." });
+      return;
+    }
+    res.status(200).json({ "status": "Bad Request", "message": "Credenciales incorrectas." })
+    return
+  } catch (error) {
+    console.log("entro", error);
+    res.status(500).send("Internal Server Error: " + error.message);
+  }
+};
 
 export const methods = {
-    checkLogin
-}
+  checkLogin,
+};
