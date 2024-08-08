6007e576dc8eec1f0d69fd7860624017e788e7a0
Carlos Sanchez Carrascal <carlosstqm2225@gmail.com>
2024-08-08 15:31:48 -0500

    changrs

--- src/routes/supervisor.route.js
+++ src/routes/supervisor.route.js
@@ -9,10 +9,10 @@ tokensMethods.isAuthorized(router, ["director", "supervisor"])
 router.get('/cedula/:cedula', supervisorMethods.getSupervisorByCedula)
 
 tokensMethods.isAuthorized(router, ["director"])
-router.get("/", supervisorMethods.getSupervisores)
-router.get("/:cedula", supervisorMethods.getSupervisorIdByCedula)
-router.post("/save", supervisorMethods.saveSupervisor)
+router.get("/", supervisorMethods.getSupervisores) // ✅
+router.get("/:cedula", supervisorMethods.getSupervisorIdByCedula) // ✅
+router.post("/save", supervisorMethods.saveSupervisor) // ✅
 router.patch("/update/:cedula", supervisorMethods.updateSupervisor)
-router.delete('/delete/:cedula', supervisorMethods.deleteSupervisor)
+router.delete('/delete/:cedula', supervisorMethods.deleteSupervisor) // ✅
 
 export default router;
