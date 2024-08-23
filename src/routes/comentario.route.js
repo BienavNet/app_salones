4c1ac61420bbd5a30d38082959c392a4b599dc93
Carlos Sanchez Carrascal <carlosstqm2225@gmail.com>
2024-08-23 16:52:24 -0500

    more changes//�

--- src/routes/comentario.route.js
+++ src/routes/comentario.route.js
@@ -5,15 +5,15 @@ import { tokensMethods } from "../functions.js";
 const router = Router();
 
 tokensMethods.isAuthorized(router, ["docente", "director"])
-router.get("/docente/:cedula", comentarioMethods.getComentarioByDocente)
+router.get("/docente/:cedula", comentarioMethods.getComentarioByDocente)//👀
 router.post("/register", comentarioMethods.registerComentario)
 router.delete("/delete/docente/:cedula", comentarioMethods.deleteAllComentariosByDocente)
 router.delete("/delete/:id", comentarioMethods.deleteComentarioById)
 
 tokensMethods.isAuthorized(router, ["director"])
-router.get("/:id", comentarioMethods.getComentarioById)
-router.get("/", comentarioMethods.getAllComentarios)
-router.get("/salon/:salon", comentarioMethods.getComentarioBySalon)
+router.get("/:id", comentarioMethods.getComentarioById) // ✅
+router.get("/", comentarioMethods.getAllComentarios) // ✅
+router.get("/salon/:salon", comentarioMethods.getComentarioBySalon)//👀
 
 
 export default router
--- src/routes/reporte.route.js
+++ src/routes/reporte.route.js
@@ -7,12 +7,20 @@ const router = Router();
 tokensMethods.isAuthorized(router, ["supervisor", "director"])
 router.get("/supervisor/:id", reporteMethods.getReporteBySupervisor)
 router.post("/register", reporteMethods.registrarReporte)
-router.post("/update/:id", reporteMethods.updateReporte)
+router.patch("/update/:id", reporteMethods.updateReporte)
 
 tokensMethods.isAuthorized(router, ["director"])
-router.get("/", reporteMethods.getReportes)
-router.get("/clase/:clase", reporteMethods.getReporteByClase)
-router.get("/salon/:salon", reporteMethods.getReporteBySalon)
+router.get("/", reporteMethods.getReportes) // ✅
+router.get("/clase/:clase", reporteMethods.getReporteByClase) //👀
+router.get("/salon/:salon", reporteMethods.getReporteBySalon) //👀
 router.delete("/delete/:id", reporteMethods.deleteReporte)
+
+
+router.get("/statistics/docente-mas-comentarios", reporteMethods.getDocenteQMasComentariosHaRealizado)  // ✅
+router.get("/statistics/salon-mas-comentarios", reporteMethods.getsalonMasComentarioTiene)  // ✅
+router.get("/statistics/salon-mas-utilizado", reporteMethods.getSalonMasUtilizado)  // ✅
+router.get("/statistics/salon-menos-utilizado", reporteMethods.getSalonMenosUtilizado)  // ✅
+router.get("/statistics/cantidad-dias-asignado", reporteMethods.getCantidadDiaMasAsignado) // ✅
+router.get("/statistics/hours-mas-frecuente", reporteMethods.getRangeHoursMasFrecuente) // ✅
 
 export default router
