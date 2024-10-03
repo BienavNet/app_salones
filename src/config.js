96090935ef02bbd7b075f16cdd5216f60346c069
Carlos Sanchez Carrascal <carlosstqm2225@gmail.com>
2024-10-03 10:20:25 -0500

    .

--- src/config.js
+++ src/config.js
@@ -1,19 +1,17 @@
 import { config } from "dotenv";
 
-if(process.env.NODE_ENV !== "production"){
-    config();
+if (process.env.NODE_ENV !== "production") {
+  config();
 }
 
 export default {
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    username: process.env.USER,
-    password: process.env.PASSWORD,
-    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-    JWT_REFRESH_SECRET_KEY: process.env.JWT_REFRESH_SECRET_KEY
-}
+  host: process.env.NODE_ENV === "production" ? process.env.MYSQLHOST : process.env.HOST,
+  database: process.env.NODE_ENV === "production" ? process.env.MYSQLDATABASE : process.env.DATABASE,
+  username: process.env.NODE_ENV === "production" ? process.env.MYSQLUSER : process.env.USER,
+  password: process.env.NODE_ENV === "production" ? process.env.MYSQLPASSWORD : process.env.PASSWORD,
+  port: process.env.NODE_ENV === "production" ? process.env.MYSQLPORT : 3306,
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  JWT_REFRESH_SECRET_KEY: process.env.JWT_REFRESH_SECRET_KEY,
+};
 
-export const {
-    PORT = 5000,
-    SALTROUNDS = 10
-} = process.env
+export const { PORT = 5000, SALTROUNDS = 10 } = process.env;
