2e0a36d37d302dd70cd2936b9243ccbb035df10d
Carlos Sanchez Carrascal <carlosstqm2225@gmail.com>
2024-10-03 09:25:53 -0500

    .

--- src/config.js
+++ src/config.js
@@ -1,12 +1,14 @@
 import { config } from "dotenv";
 
-config();
+if(process.env.NODE_ENV !== "production"){
+    config();
+}
 
 export default {
-    host: process.env.HOST || "",
-    database: process.env.DATABASE || "",
-    username: process.env.USER || "",
-    password: process.env.PASSWORD || "",
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    username: process.env.USER,
+    password: process.env.PASSWORD,
     JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
     JWT_REFRESH_SECRET_KEY: process.env.JWT_REFRESH_SECRET_KEY
 }
