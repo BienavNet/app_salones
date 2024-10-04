41af6909817e4cc55fb22756a13c320c7b3d4225
Bienavnet <bienavnet@gmail.com>
2024-10-03 21:40:59 -0500

    more changes

--- src/config.js
+++ src/config.js
@@ -5,11 +5,15 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 export default {
-  host: process.env.NODE_ENV === "production" ? process.env.MYSQLHOST : process.env.HOST,
-  database: process.env.NODE_ENV === "production" ? process.env.MYSQLDATABASE : process.env.DATABASE,
-  username: process.env.NODE_ENV === "production" ? process.env.MYSQLUSER : process.env.USER,
-  password: process.env.NODE_ENV === "production" ? process.env.MYSQLPASSWORD : process.env.PASSWORD,
-  port: process.env.NODE_ENV === "production" ? process.env.MYSQLPORT : 3306,
+  // host: process.env.NODE_ENV === "production" ? process.env.MYSQLHOST : process.env.HOST,
+  // database: process.env.NODE_ENV === "production" ? process.env.MYSQLDATABASE : process.env.DATABASE,
+  // username: process.env.NODE_ENV === "production" ? process.env.MYSQLUSER : process.env.USER,
+  // password: process.env.NODE_ENV === "production" ? process.env.MYSQLPASSWORD : process.env.PASSWORD,
+  // port: process.env.NODE_ENV === "production" ? process.env.MYSQLPORT : 3306,
+  host: process.env.MYSQLDATABASE,
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  port: process.env.MYSQLPORT,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   JWT_REFRESH_SECRET_KEY: process.env.JWT_REFRESH_SECRET_KEY,
 };
--- src/database/database.js
+++ src/database/database.js
@@ -2,36 +2,12 @@ import mysql from "mysql2/promise";
 import config from "../config.js";
 
 const connection = await mysql.createConnection({
-    // host: config.host,
-    // database: config.database,
-    // user: config.username,
-    // password: config.password,
-    // port: config.port
-    host: 'junction.proxy.rlwy.net',
-    database: 'app',
-    user: 'root',
-    password: 'uCvrZtWWgWPXSXxezuoOoDDZFtcUBPmM',
-    port: '19664'
+    host: config.host,
+    database: config.database,
+    user: config.username,
+    password: config.password,
+    port: config.port
 })
-
-// const connection = ()=> {
-//     return new Promise(async (resolve, reject) => {
-//         let connection = await mysql.createConnection({
-//             host: config.host,
-//             database: config.database,
-//             user: config.username,
-//             password: config.password
-//         })
-
-//         connection.connect(err => {
-//             if(err){
-//                 reject(err)
-//             }else{
-//                 resolve(true)
-//             }
-//         })
-//     })
-// }
 
 const getConnection = ()=> {
    return connection
