aaf4cd3427a2821117e0d616aeaf3e45f61031f7
Bienavnet <bienavnet@gmail.com>
2024-10-03 21:19:42 -0500

    database connection test

--- src/database/database.js
+++ src/database/database.js
@@ -1,32 +1,37 @@
 import mysql from "mysql2/promise";
 import config from "../config.js";
 
-// const connection = await mysql.createConnection({
-//     host: config.host,
-//     database: config.database,
-//     user: config.username,
-//     password: config.password,
-//     port: config.port
-// })
+const connection = await mysql.createConnection({
+    // host: config.host,
+    // database: config.database,
+    // user: config.username,
+    // password: config.password,
+    // port: config.port
+    host: 'junction.proxy.rlwy.net',
+    database: 'app',
+    user: 'root',
+    password: 'uCvrZtWWgWPXSXxezuoOoDDZFtcUBPmM',
+    port: '19664'
+})
 
-const connection = ()=> {
-    return new Promise(async (resolve, reject) => {
-        let connection = await mysql.createConnection({
-            host: config.host,
-            database: config.database,
-            user: config.username,
-            password: config.password
-        })
+// const connection = ()=> {
+//     return new Promise(async (resolve, reject) => {
+//         let connection = await mysql.createConnection({
+//             host: config.host,
+//             database: config.database,
+//             user: config.username,
+//             password: config.password
+//         })
 
-        connection.connect(err => {
-            if(err){
-                reject(err)
-            }else{
-                resolve(true)
-            }
-        })
-    })
-}
+//         connection.connect(err => {
+//             if(err){
+//                 reject(err)
+//             }else{
+//                 resolve(true)
+//             }
+//         })
+//     })
+// }
 
 const getConnection = ()=> {
    return connection
