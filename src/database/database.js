67534465e340c859b75eae644914882fb3bd5d7e
Bienavnet <bienavnet@gmail.com>
2024-10-03 22:02:42 -0500

    ...

--- src/database/database.js
+++ src/database/database.js
@@ -2,11 +2,17 @@ import mysql from "mysql2/promise";
 import config from "../config.js";
 
 const connection = await mysql.createConnection({
-    host: config.host,
-    database: config.database,
-    user: config.username,
-    password: config.password,
-    port: config.port
+    // host: config.host,
+    // database: config.database,
+    // user: config.username,
+    // password: config.password,
+    // port: config.port
+
+    host: 'junction.proxy.rlwy.net',
+    database: 'app',
+    user: 'root',
+    password: 'uCvrZtWWgWPXSXxezuoOoDDZFtcUBPmM',
+    port: '19664'
 })
 
 const getConnection = ()=> {
