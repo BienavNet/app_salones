9723322d422e9bc236d14377883714de0478b35b
Bienavnet <bienavnet@gmail.com>
2024-10-03 20:48:57 -0500

    database connection update

--- src/database/database.js
+++ src/database/database.js
@@ -1,13 +1,32 @@
 import mysql from "mysql2/promise";
 import config from "../config.js";
 
-const connection = await mysql.createConnection({
-    host: config.host,
-    database: config.database,
-    user: config.username,
-    password: config.password,
-    port: config.port
-})
+// const connection = await mysql.createConnection({
+//     host: config.host,
+//     database: config.database,
+//     user: config.username,
+//     password: config.password,
+//     port: config.port
+// })
+
+const connection = ()=> {
+    return new Promise((resolve, reject) => {
+        let connection = mysql.createConnection({
+            host: config.host,
+            database: config.database,
+            user: config.username,
+            password: config.password
+        })
+
+        connection.connect(err => {
+            if(err){
+                reject(err)
+            }else{
+                resolve(true)
+            }
+        })
+    })
+}
 
 const getConnection = ()=> {
    return connection
