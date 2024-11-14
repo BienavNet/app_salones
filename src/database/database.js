2098905130c810c2350ec798f88cff3a49ebed45
Carlos Sanchez Carrascal <carlosstqm2225@gmail.com>
2024-11-14 17:14:02 -0500

    .

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,6 @@
 import { createPool } from "mysql2/promise";
 import { DATABASE, HOST, MYSQLPORT, PASSWORD, USER } from "../config.js";
-
+console.log("config js DATABASE, HOST, MYSQLPORT, PASSWORD, USER", DATABASE, HOST, MYSQLPORT, PASSWORD, USER)
 export const connection = createPool({
   host: HOST,
   database:DATABASE,
@@ -9,4 +9,14 @@ export const connection = createPool({
   port:MYSQLPORT,
 });
 
-if (!connection){ throw Error("Errror al conectarse con la base de datos")}
+async function checkConnection() {
+  try {
+    await connection.getConnection();
+    console.log("Conexión a la base de datos establecida correctamente.");
+  } catch (error) {
+    console.error("Error al conectarse a la base de datos:", error.message);
+    throw new Error("Error al conectarse a la base de datos");
+  }
+}
+
+checkConnection();
