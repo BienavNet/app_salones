7f61c05d03d67005ff0a9e5e5c9dff4f9977e3be
Carlos Sanchez Carrascal <carlosstqm2225@gmail.com>
2024-11-15 17:32:19 -0500

    delete console.log

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,5 @@
 import { createPool } from "mysql2/promise";
 import { DATABASE, HOST, MYSQLPORT, PASSWORD, USER } from "../config.js";
-console.log("config js DATABASE, HOST, MYSQLPORT, PASSWORD, USER", DATABASE, HOST, MYSQLPORT, PASSWORD, USER)
 export const connection = createPool({
   host: HOST,
   database:DATABASE,
