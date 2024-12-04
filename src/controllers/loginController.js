4f36d5f6d87b2ae1f20c48ab65a75403d705b56e
Carlos Sanchez Carrascal <carlosstqm2225@gmail.com>
2024-12-04 14:52:54 -0500

    login success

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,8 +1,7 @@
 import { Validaciones } from "../assets/validation.js";
 import { connection } from "./../database/database.js";
 import { tokensMethods as tokens } from "./../functions.js";
-
-
+import bcrypt from "bcryptjs";
 // Revisa las variables y las consulta en la respectiva tabla, dependiendo del rol y si coincide todo envia un mensaje de respuesta correcto y crea los token y las cookies para permitir el inicio de sesion
 const checkLogin = async (req, res) => {
   if (
@@ -18,7 +17,6 @@ const checkLogin = async (req, res) => {
 
   try {
     const { correo, contrasena, rol } = req.body;
-    // Validaciones
     try {
       Validaciones.correo(correo);
       Validaciones.contrasena(contrasena);
@@ -31,64 +29,85 @@ const checkLogin = async (req, res) => {
 
     // Ejecutar la consulta para verificar usuario
     const query =
-      "SELECT id, nombre, cedula FROM persona WHERE correo = ? AND contrasena = ?";
-    const [result] = await connection.query(query, [correo, contrasena]);
+      "SELECT id, nombre, cedula, contrasena FROM persona WHERE correo = ?";
+    const [result] = await connection.query(query, [correo]);
 
     // Verificar si se encontró algún usuario
     if (result.length > 0) {
-      const { id, nombre, cedula } = result[0];
+      const { id, nombre, cedula, contrasena: hashedPassword } = result[0];
+      if (!id || !hashedPassword) {
+        return res.status(401).json({ status: "error", message: "Credenciales incorrectas." });
+      }
+      // Verificar si la contraseña está en texto plano o encriptada
+      let isPasswordCorrect = false;
+      // Si el hash de la contraseña almacenado en la base de datos empieza con "$2a$10" significa que es un hash bcrypt
+      if (hashedPassword.startsWith('$2a$10')) {
+        isPasswordCorrect = await bcrypt.compare(contrasena, hashedPassword);
+      } else {
+        isPasswordCorrect = contrasena === hashedPassword;
+      }
 
-      if (id !== undefined && id !== "") {
-        // Ejecutar la consulta para verificar el rol
-        const roleQuery = `SELECT id AS id_rol FROM ${rol} WHERE persona = ?`;
-        const [roleResult] = await connection.query(roleQuery, [id]);
+      // Responder según el tipo de contraseña (texto plano o hash bcrypt)
+      if (!isPasswordCorrect) {
+        return res.status(401).json({
+          status: "error",
+          message: hashedPassword.startsWith('$2a$10') ? "Credenciales incorrectas." : "La contraseña no está encriptada correctamente.",
+        });
+      }
 
-        if (roleResult.length > 0) {
-          const { id_rol } = roleResult[0];
-          let directorId;
-          if (["docente", "supervisor"].includes(rol)) {
-            const directorQuery = "SELECT persona FROM director LIMIT 1";
-            const [directorResult] = await connection.query(directorQuery);
-            if (directorResult.length > 0) {
-              directorId = directorResult[0].persona;
-            }
+      // Verificar el rol
+      const roleQuery = `SELECT id AS id_rol FROM ${rol} WHERE persona = ?`;
+      const [roleResult] = await connection.query(roleQuery, [id]);
 
-          }
-          if (id_rol !== undefined && id_rol !== "") {
-            // Generar el token
-            const token = tokens.signToken({
-              id,
-              cedula,
-              nombre,
-              user: correo,
-              rol,
-              directorId
-            });
+      if (roleResult.length > 0) {
+        const { id_rol } = roleResult[0];
+        let directorId;
 
-            // Configurar las cookies y la cabecera de autorización
-            res.set({ Authorization: token });
-            res.cookie("access_token", token, {
-              httpOnly: true,
-              sameSite: "strict",
-              maxAge: 1000 * 60 * 60, // 1 hora
-            });
-
-            return res.status(200).json({
-              status: "ok",
-              message: "Login Correcto.",
-              access_token: token,
-            });
+        // Si el rol es docente o supervisor, obtener el director
+        if (["docente", "supervisor"].includes(rol)) {
+          const directorQuery = "SELECT persona FROM director LIMIT 1";
+          const [directorResult] = await connection.query(directorQuery);
+          if (directorResult.length > 0) {
+            directorId = directorResult[0].persona;
           }
         }
 
-        return res
-          .status(400)
-          .json({ status: "error", message: "Error al determinar el rol." });
+        if (id_rol) {
+          // Generar el token
+          const token = tokens.signToken({
+            id,
+            cedula,
+            nombre,
+            user: correo,
+            rol,
+            directorId,
+          });
+
+          // Configurar las cookies y la cabecera de autorización
+          res.set({ Authorization: token });
+          res.cookie("access_token", token, {
+            httpOnly: true,
+            sameSite: "strict",
+            maxAge: 1000 * 60 * 60 * 24 * 30, // 30 días
+          });
+
+          return res.status(200).json({
+            status: "ok",
+            message: "Login correcto.",
+            access_token: token,
+          });
+        } else {
+          return res.status(400).json({
+            status: "error",
+            message: "Error al determinar el rol.",
+          });
+        }
       }
     } else {
-      return res
-        .status(401)
-        .json({ status: "Bad Request", message: "Credenciales incorrectas." });
+      return res.status(401).json({
+        status: "error",
+        message: "Credenciales incorrectas.",
+      });
     }
   } catch (error) {
     return res.status(500).send("Internal Server Error: " + error.message);
